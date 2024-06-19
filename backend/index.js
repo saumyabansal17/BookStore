b4fd796e1c557dd2e174a5519ae6db72cf7d7ad9
saumyabansal17 <saumyabansal17@gmail.com>
2024-06-19 17:58:20 +0530

    done with basic backend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
-// import { PORT, mongoDBURL } from './config.js';
-// import connectDB from "./db/connect.js";
-// const PORT = process.env.PORT || 5555;
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5555;
 const mongoDBURL=process.env.DB;
 
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
 app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(200).send('Hello SAUMYA here');
+  console.log(request);
+  return response.status(200).send('Hello SAUMYA here');
 });
+
+app.use('/books', booksRoute);
 
 mongoose
 .connect(mongoDBURL)
