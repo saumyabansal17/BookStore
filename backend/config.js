d156508c83a9f992542013d70c7bbdaacf1331a4
saumyabansal17 <saumyabansal17@gmail.com>
2024-06-19 14:45:21 +0530

    database connection done

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
-export const PORT=5555;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGODB_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,55 @@
 import express from 'express';
-import {PORT} from './config.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app=express();
+// import { PORT, mongoDBURL } from './config.js';
+// import connectDB from "./db/connect.js";
+// const PORT = process.env.PORT || 5555;
+const app = express();
+dotenv.config();
 
-app.listen(PORT, ()=>{
-    console.log(`App is listening to port : ${PORT}`);
+const PORT = process.env.PORT || 5555;
+const mongoDBURL=process.env.DB;
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(200).send('Hello SAUMYA here');
 });
+
+mongoose
+.connect(mongoDBURL)
+.then(() =>{
+  console.log("Db connected successfully");
+  app.listen(PORT, () => {
+                console.log(`App is listening on port: ${PORT}`);
+               });
+})
+.catch((error)=>{
+  console.log(error);
+})
+
+// const start = async()=>{
+//   try{
+//         await connectDB();
+//          console.log('App connected to database');
+//          app.listen(PORT, () => {
+//            console.log(`App is listening on port: ${PORT}`);
+//          });
+//         }
+//   catch(error){
+//     console.log(error);
+//   }
+// }
+
+// start();
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(() => {
+//     console.log('App connected to database');
+//     app.listen(PORT, () => {
+//       console.log(`App is listening on port: ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to the database:', error);
+//   });
